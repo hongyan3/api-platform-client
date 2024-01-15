ac3702ed4ab24bfa55ab8b11c88fb207351ec7da
xiyuan <xiyuan230@qq.com>
2024-01-15 22:56:34 +0800

    feat: 修改slogon，logo，移除更多登陆方式

--- src/pages/User/Login/index.tsx
+++ src/pages/User/Login/index.tsx
@@ -168,19 +168,19 @@ const Login: React.FC = () => {
             maxWidth: '75vw',
           }}
           logo={<img alt="logo" src="/logo.svg" />}
-          title="Ant Design"
+          title="API Open Platform"
           subTitle={intl.formatMessage({ id: 'pages.layouts.userLayout.title' })}
           initialValues={{
             autoLogin: true,
           }}
-          actions={[
-            <FormattedMessage
-              key="loginWith"
-              id="pages.login.loginWith"
-              defaultMessage="其他登录方式"
-            />,
-            <ActionIcons key="icons" />,
-          ]}
+          // actions={[
+          //   <FormattedMessage
+          //     key="loginWith"
+          //     id="pages.login.loginWith"
+          //     defaultMessage="其他登录方式"
+          //   />,
+          //   <ActionIcons key="icons" />,
+          // ]}
           onFinish={async (values) => {
             await handleSubmit(values as API.UserVO);
           }}
